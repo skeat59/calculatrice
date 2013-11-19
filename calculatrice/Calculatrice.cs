c444f7187ed0aa434a35b06afbc9eb13c4d748d9
skeat59 <gdalia_t@epita.fr>
2013-11-19 11:47:49 +0100

    test

---
+++ calculatrice/Calculatrice.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+
+namespace calculatrice
+{
+    class Calculatrice
+    {
+        public static long Simple_operation(long operand1, char operation, long operand2)
+        {
+            long result;
+            switch (operation )
+            {
+                case '+' :
+                    result = operand1 + operand2;
+                    break;
+                case '-':
+                    result = operand1 - operand2;
+                    break;  
+                case '*':
+                    result = operand1 * operand2;
+                    break;
+                case '/':
+                    result = operand1/operand2;
+                    break;
+                case '%':
+                    result = operand1%operand2;
+                    break;
+                default :
+                    result = 42;
+                    break;
+
+            }
+            return result;
+
+        }
+
+        public static long Pow(int x, int n)
+        {
+            long result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                result = result*x;
+            }
+            return result;
+        }
+
+        public static double Fibo(uint n)
+        {
+            double result;
+            if (n == 0)
+            {
+                result = 0;
+            }
+            else
+            {
+                if (n == 1)
+                {
+                    result = 1;
+                }
+                else
+                {
+                    result = Fibo(n - 2) + Fibo(n - 1);
+                }
+            }
+            return result;
+        }
+
+        public static double Factorial(uint n)
+        {
+            double result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                result = result*n;
+                n = n - 1;
+            }
+            return result;
+        }
+
+        public static double abs(double x)
+        {
+            if (x < 0)
+            {
+                return (- x);
+            }
+            else
+            {
+                return x;
+            }
+        }
+        
+       /* public static double Sqrt(double n, double x)
+        {
+            float precision = 0.00001f;
+            int xi = Convert.ToInt32(x);
+            long xca = Pow(xi, 2);
+            double res = 0;
+            if ((n- xca) >= precision  )
+            {
+                Sqrt(n,((n - x)/x)/2);
+            }
+            
+                return x; 
+            
+           
+        }*/
+    }
+}
---
+++ calculatrice/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calculatrice
+{
+    public partial class Form1 : Form
+    {
+        double [] operands = new double[2] {0, 0};
+        int current_operand = 0;
+        char operation = '+';
+        double result = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox.Text = operands[current_operand].ToString();
+        }
+
+        private void Disable_operation_buttons()
+        {
+            this.button_plus.Enabled = false;
+            this.button_minus.Enabled = false;
+            this.button_mul.Enabled = false;
+            this.button_div.Enabled = false;
+            this.button_mod.Enabled = false;
+            this.button_pow.Enabled = false;
+            this.button_sqrt.Enabled = false;
+            this.button_fibo.Enabled = false;
+            this.button_fact.Enabled = false;
+        }
+
+        private void operand_updated(object sender, EventArgs e)
+        {
+            this.textBox.Text = operands[current_operand].ToString();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            if (operands[current_operand] == 0)
+                result = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            operands[current_operand]++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            operands[current_operand] += 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            operands[current_operand] += 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            operands[current_operand] += 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            operands[current_operand] += 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            operands[current_operand] += 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            operands[current_operand] += 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            operands[current_operand] += 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            operands[current_operand] *= 10;
+            operands[current_operand] += 9;
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            if (current_operand == 0)
+                current_operand++;
+            if (operands[0] == 0)
+                operands[0] = result;
+
+            operation = '+';
+            Disable_operation_buttons();
+        }
+
+        private void button_moins_Click(object sender, EventArgs e)
+        {
+            if (current_operand == 0)
+                current_operand++;
+            if (operands[0] == 0)
+                operands[0] = result;
+
+            operation = '-';
+            Disable_operation_buttons();
+        }
+
+        private void button_mul_Click(object sender, EventArgs e)
+        {
+            if (current_operand == 0)
+                current_operand++;
+            if (operands[0] == 0)
+                operands[0] = result;
+
+            operation = '*';
+            Disable_operation_buttons();
+        }
+
+        private void button_div_Click(object sender, EventArgs e)
+        {
+            if (current_operand == 0)
+                current_operand++;
+            if (operands[0] == 0)
+                operands[0] = result;
+
+            operation = '/';
+            Disable_operation_buttons();
+        }
+
+        private void button_mod_Click(object sender, EventArgs e)
+        {
+            if (current_operand == 0)
+                current_operand++;
+            if (operands[0] == 0)
+                operands[0] = result;
+
+            operation = '%';
+            Disable_operation_buttons();
+        }
+
+        private void button_pow_Click(object sender, EventArgs e)
+        {
+            if (current_operand == 0)
+                current_operand++;
+            if (operands[0] == 0)
+                operands[0] = result;
+
+            operation = '^';
+            Disable_operation_buttons();
+        }
+
+        private void button_sqrt_Click(object sender, EventArgs e)
+        {
+            if (operands[0] == 0)
+                operands[0] = result;
+
+            operation = 's';
+            Egal();
+        }
+
+        private void button_fibo_Click(object sender, EventArgs e)
+        {
+            if (operands[0] == 0)
+                operands[0] = result;
+
+            operation = 'f';
+            Egal();
+        }
+
+        private void button_fact_Click(object sender, EventArgs e)
+        {
+            if (operands[0] == 0)
+                operands[0] = result;
+
+            operation = '!';
+            Egal();
+        }
+
+        private void button_egal_Click(object sender, EventArgs e)
+        {
+            Egal();
+        }
+
+        private void Egal()
+        {
+            if (operands[0] == 0 && operands[1] == 0)
+                return;//several clicks on = button
+
+            switch (operation)
+            {
+                case '^':
+                    result = Calculatrice.Pow((int)operands[0], (int)operands[1]);
+                    break;
+                /*case 's':
+                    result = Calculatrice.Sqrt((float)operands[0], (float)operands[0]);
+                    break;*/
+                case 'f':
+                    result = Calculatrice.Fibo((uint)Math.Abs(operands[0]));
+                    break;
+                case '!':
+                    result = Calculatrice.Factorial((uint)Math.Abs(operands[0]));
+                    break;
+                default:
+                    result = Calculatrice.Simple_operation((long)operands[0], operation, (long)operands[1]);
+                    break;
+            }
+
+            current_operand = 0;
+            this.textBox.Text = result.ToString();
+            operands[0] = 0;
+            operands[1] = 0;
+
+            this.button_plus.Enabled = true;
+            this.button_minus.Enabled = true;
+            this.button_mul.Enabled = true;
+            this.button_div.Enabled = true;
+            this.button_mod.Enabled = true;
+            this.button_pow.Enabled = true;
+            this.button_sqrt.Enabled = true;
+            this.button_fibo.Enabled = true;
+            this.button_fact.Enabled = true;
+        }
+    }
+}
